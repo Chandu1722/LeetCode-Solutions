b533f4c326f3f7b23c45c91abfc90055ce111075
Chandu1722 <chendichandu.com@gmail.com>
2025-03-21 19:17:45 +0530

    3285. Find Indices of Stable Mountains

---
+++ Array-LeetCode/height.java
@@ -0,0 +1,11 @@
+class Solution {
+    public List<Integer> stableMountains(int[] height, int threshold) {
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 1; i < height.length; i++) {
+            int n = height[i - 1];
+            if (n > threshold && i != 0)
+                arr.add(i);
+        }
+        return arr;
+    }
+}
