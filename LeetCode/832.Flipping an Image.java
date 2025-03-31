3505f494002053feae42fae8970f1874a6a7a6dc
Chandu1722 <chendichandu.com@gmail.com>
2025-03-31 22:51:04 +0530

    2025-03-31 LeetCide

---
+++ LeetCode/832.Flipping an Image.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        int i, j;
+        for (i = 0; i < image.length; i++) {
+            int l = 0;
+            int r = image[0].length - 1;
+            while (l <= r) {
+                int t = image[i][l];
+                image[i][l] = image[i][r];
+                image[i][r] = t;
+                l++;
+                r--;
+            }
+        }
+        for (i = 0; i < image.length; i++) {
+            for (j = 0; j < image[0].length; j++) {
+                if (image[i][j] == 0) {
+                    image[i][j] = 1;
+                } else {
+                    image[i][j] = 0;
+                }
+            }
+        }
+        return image;
+    }
+}
