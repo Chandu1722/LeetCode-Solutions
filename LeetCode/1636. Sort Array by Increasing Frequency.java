579a2893b068e21f1cb2c87c1241fadee66460bb
Chandu1722 <chendichandu.com@gmail.com>
2025-05-14 12:39:55 +0530

    2025-05-14 LeetCode

---
+++ LeetCode/1636. Sort Array by Increasing Frequency.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] frequencySort(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (map.get(nums[i]) > map.get(nums[j])
+                        || (map.get(nums[i]) == map.get(nums[j]) && nums[i] < nums[j])) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+}
