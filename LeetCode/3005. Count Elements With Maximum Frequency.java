ef13f9036317da6ce98b72558da9b042acfab2f4
Chandu1722 <chendichandu.com@gmail.com>
2025-05-18 08:57:56 +0530

    2025-05-18 LeetCode

--- LeetCode/3005. Count Elements With Maximum Frequency.java
+++ LeetCode/3005. Count Elements With Maximum Frequency.java
@@ -6,7 +6,7 @@ class Solution {
         }
         int a = 0, b = 0;
         for (Integer key : map.keySet()) {
-            a = Math.max(a, map.get(key));
+            a = Math.max(a, map.get(key)); 
         }
         for (Integer key : map.keySet()) {
             if (map.get(key) == a) {
