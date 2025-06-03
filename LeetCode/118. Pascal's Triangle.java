d4549d21718e17501d2262fa959febc23807e0f7
Chandu1722 <chendichandu.com@gmail.com>
2025-06-03 19:39:00 +0530

    2025-06-03 LeetCode

---
+++ LeetCode/118. Pascal's Triangle.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<Integer>> generate(int n) {
+        List<List<Integer>> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            List<Integer> row = new ArrayList<>();
+            int num = 1;
+            for (int j = 0; j <= i; j++) {
+                row.add(num);
+                num = num * (i - j) / (j + 1);
+            }
+            arr.add(row);
+        }
+        return arr;
+    }
+}
