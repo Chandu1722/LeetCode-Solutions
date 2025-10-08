2e6a6922bc60343ae431694802f4cd74f72b58f0
Chandu1722 <chendichandu.com@gmail.com>
2025-10-08 19:43:59 +0530

    2025-10-08 LeetCode

---
+++ LeetCode/648. Replace Words.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String replaceWords(List<String> dict, String sentence) {
+        Set<String> set = new HashSet<>(dict);
+        StringBuilder ans = new StringBuilder();
+        List<String> list = new ArrayList<>(
+                Arrays.asList(sentence.split(" ")));
+        for (String t : list) {
+            int i = 0;
+            while (i <= t.length()) {
+                String curr = t.substring(0, i++);
+                if (set.contains(curr)) {
+                    ans.append(curr).append(" ");
+                    break;
+                }
+                if (i == t.length() + 1) {
+                    ans.append(t).append(" ");
+                }
+            }
+        }
+        ans.deleteCharAt(ans.length() - 1);
+        return ans.toString();
+    }
+}
