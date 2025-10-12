b782c7d2c617ef9ff88735819de680d60ad14912
Chendi Chandu <chendichandu.com@gmail.com>
2025-10-12 21:10:03 +0530

    2

---
+++ LeetCode/877. Stone Game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        // Arrays.sort(piles);
+        // int i=0;
+        // int j=piles.length-1;
+        // int a=0,b=0;
+        // while(i<j){
+        //     a+=piles[j];
+        //     b+=piles[i];
+        //     i++;
+        //     j--;
+        // }
+        // return a>b;
+        return true;
+    }
+}
