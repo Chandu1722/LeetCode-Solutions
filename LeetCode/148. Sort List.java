f447324a7dd9f83f24258bae21eac5c31bcd1a67
Chendi Chandu <chendichandu.com@gmail.com>
2025-10-23 20:40:33 +0530

    2025-10-23 LeetCode

---
+++ LeetCode/148. Sort List.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        List<Integer> list = new ArrayList<>();
+        ListNode temp = head;
+        while (temp != null) {
+            list.add(temp.val);
+            temp = temp.next;
+        }
+        Collections.sort(list);
+        head = new ListNode(list.get(0));
+        temp = head;
+        for (int i = 1; i < list.size(); i++) {
+            temp.next = new ListNode(list.get(i));
+            temp = temp.next;
+        }
+        return head;
+    }
+}
