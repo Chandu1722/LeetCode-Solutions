04160d2234f13fef2f1f147dc549a281ff62ddd1
Chendi Chandu <chendichandu.com@gmail.com>
2025-11-26 18:10:16 +0530

    Fix formatting in Pascal's Triangle solution

--- LeetCode/118. Pascal's Triangle.java
+++ LeetCode/118. Pascal's Triangle.java
@@ -1,7 +1,7 @@
  class Solution {
     public List<List<Integer>> generate(int n) {
         List<List<Integer>> arr = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) { 
             List<Integer> row = new ArrayList<>();
             int num = 1;
             for (int j = 0; j <= i; j++) { 
