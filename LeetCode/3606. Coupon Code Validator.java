c2b901193e21aa86b454261865be97a91f84de62
Chendi Chandu <chendichandu.com@gmail.com>
2025-12-13 09:42:04 +0530

    2025-12-13 LeetCode

---
+++ LeetCode/3606. Coupon Code Validator.java
@@ -0,0 +1,46 @@
+class Solution {
+    public List<String> validateCoupons(String[] code, String[] businessLine, boolean[] isActive) {
+        List<String> ans = new ArrayList<>();
+        List<String> e = new ArrayList<>();
+        List<String> g = new ArrayList<>();
+        List<String> p = new ArrayList<>();
+        List<String> r = new ArrayList<>();
+        for (int i = 0; i < isActive.length; i++) {
+            if (isActive[i]) {
+                if (businessLine[i].equals("electronics") || businessLine[i].equals("grocery")
+                        || businessLine[i].equals("pharmacy")
+                        || businessLine[i].equals("restaurant")) {
+                    if (code[i].length() == 0)
+                        continue;
+                    boolean found = true;
+                    for (int j = 0; j < code[i].length(); j++) {
+                        char c = code[i].charAt(j);
+                        if (!(Character.isLetterOrDigit(c)) && (c != '_')) {
+                            found = false;
+                            break;
+                        }
+                    }
+                    if (found) {
+                        if (businessLine[i].startsWith("e"))
+                            e.add(code[i]);
+                        if (businessLine[i].startsWith("g"))
+                            g.add(code[i]);
+                        if (businessLine[i].startsWith("p"))
+                            p.add(code[i]);
+                        if (businessLine[i].startsWith("r"))
+                            r.add(code[i]);
+                    }
+                }
+            }
+        }
+        Collections.sort(e);
+        Collections.sort(g);
+        Collections.sort(p);
+        Collections.sort(r);
+        ans.addAll(e);
+        ans.addAll(g);
+        ans.addAll(p);
+        ans.addAll(r);
+        return ans;
+    }
+}
